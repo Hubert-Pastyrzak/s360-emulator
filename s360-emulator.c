a49a0f2bdb186c2204db9d6d4eb37b52f2cf88c6
Hubert-Pastyrzak <97910195+Hubert-Pastyrzak@users.noreply.github.com>
2022-12-23 17:34:53 +0100

    Implement the SPM instruction

--- s360-emulator.c
+++ s360-emulator.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 unsigned long long psw;
+unsigned int gpr[16];
+
+void write_cc(int value) {
+    psw = (psw & 0xFFFFFFF3FFFFFFFFULL) | (((unsigned long long)(value & 0x3)) << 34);
+}
+
+int read_cc() {
+    return (psw & 0x0000000C00000000ULL) >> 34;
+}
+
+void write_pm(int value) {
+    psw = (psw & 0xFFFFFF0FFFFFFFFFULL) | (((unsigned long long)(value & 0xF)) << 36);
+}
+
+int read_pm() {
+    return (psw & 0x000000F000000000ULL) >> 36;
+}
 
 void write_ia(int value) {
     psw = (psw & 0x000000FFFFFFFFFFULL) | (((unsigned long long)(value & 0xFFFFFF)) << 40);
@@ -40,12 +57,24 @@ void report_invalid_opcode_error(unsigned char opcode) {
     printf("Error: Invalid opcode 0x%x\n", opcode);
 }
 
-int execute_opcode(unsigned char opcode) {
+void spm(int r1) {
+    write_cc((gpr[r1] & 0x0C) >> 2);
+    write_pm((gpr[r1] & 0xF0) >> 4);
+}
+
+int execute_opcode(Program* program, unsigned char opcode) {
     switch (opcode) {
+        case 0x04:
+            int r1 = fetch_byte(program) & 0x0F;
+            spm(r1);
+            break;
+
         default:
             report_invalid_opcode_error(opcode);
             return 0;
     }
+
+    return 1;
 }
 
 void run(FILE* inputFile) {
@@ -54,7 +83,7 @@ void run(FILE* inputFile) {
     psw = 0x0000000000000000ULL;
     while (read_ia() < program->length) {
         unsigned char opcode = fetch_byte(program);
-        if (!execute_opcode(opcode)) {
+        if (!execute_opcode(program, opcode)) {
             free(program);
             printf("Program terminated\n");
             return;
